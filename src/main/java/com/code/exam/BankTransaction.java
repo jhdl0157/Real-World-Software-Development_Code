aa04ec22ee9655c93e47461edc9987eb3c662daa
jhdl0157 <jhdl0157@naver.com>
2022-07-23 19:35:37 +0900

    BankTransaction 클래스 생성

--- src/main/java/com/code/exam/BankTransaction.java
+++ src/main/java/com/code/exam/BankTransaction.java
@@ -1,8 +1,50 @@
 package com.code.exam;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class BankTransaction {
-    public BankTransaction(LocalDate date, double amount, String description) {
+    private final LocalDate date;
+    private final double amount;
+    private final String description;
+
+    public BankTransaction(final LocalDate date, final double amount, final String description) {
+        this.date = date;
+        this.amount=amount;
+        this.description=description;
+    }
+
+    @Override
+    public String toString() {
+        return "BankTransaction{" +
+                "date=" + date +
+                ", amount=" + amount +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BankTransaction that = (BankTransaction) o;
+        return Double.compare(that.amount, amount) == 0 && Objects.equals(date, that.date) && Objects.equals(description, that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(date, amount, description);
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
