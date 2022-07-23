0d078758954172b340760047030e6b83e52faa19
jhdl0157 <jhdl0157@naver.com>
2022-07-23 18:08:57 +0900

    특정한 월을 합계후 출력

--- src/main/java/com/code/exam/BankStatementAnalyzerSimple.java
+++ src/main/java/com/code/exam/BankStatementAnalyzerSimple.java
@@ -3,6 +3,9 @@ package com.code.exam;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class BankStatementAnalyzerSimple {
@@ -12,11 +15,15 @@ public class BankStatementAnalyzerSimple {
     public static void main(final String[] args) throws Exception {
         final Path path = Paths.get(RESOURCES + "bank-data-simple.csv");
         final List<String> lines = Files.readAllLines(path);
-        double total = 0;
+        double total = 0d;
+        final DateTimeFormatter DATE_PATTERN=DateTimeFormatter.ofPattern("dd-MM-yyy");
         for(final String line: lines) {
             String[] columns = line.split(",");
-            double amount = Double.parseDouble(columns[1]);
-            total += amount;
+            final LocalDate date=LocalDate.parse(columns[0],DATE_PATTERN);
+            if(date.getMonth() == Month.JANUARY){
+                final double amount=Double.parseDouble(columns[1]);
+                total += amount;
+            }
         }
         System.out.println("The total for all transactions is " + total);
     }
